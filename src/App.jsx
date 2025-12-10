886c08995dff88ac8943273e360da7347cf5cc15
muhammad <muhammadj110@gmail.com>
2025-12-10 12:41:38 -0500

    color change

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ const STYLES = {
   option3: {
     bg: "linear-gradient(to bottom, #efe1b6b0 0%, #d3b950d0 100%)",
     surface: "#F9F5E7",
-    // surfaceSoft: "#F1EAD1",
-    surfaceSoft: "#d3b950d0",
+    surfaceSoft: "#F1EAD1",
+    // surfaceSoft: "#d3b950d0",
     border: "#C5C1AC",
     text: "#2B2B2B",
     title: "#000000bc",
